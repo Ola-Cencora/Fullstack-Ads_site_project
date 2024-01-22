7b071ead10503467fbc4c526becb9e4c112235a9
Ola <cencora.aleksandra@gmail.com>
2024-01-22 17:57:40 +0100

    Update server and connection to db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectToDB = () => {
 
   // connect to DB
-  mongoose.connect('mongodb://0.0.0.0:27017/MessageBoardDB', { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect('mongodb://0.0.0.0:27017/MessageBoardDB', {});
   const db = mongoose.connection;
 
   // on success
--- server.js
+++ server.js
@@ -21,17 +21,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(session({ secret: process.env.sessionSecret, store: MongoStore.create(mongoose.connection), resave: false, saveUninitialized: false }));
 
-// serve static files from the React app
-//app.use(express.static(path.join(__dirname, '/client/build')));
-//app.use(express.static(path.join(__dirname, '/public')));
-
 // import routes
 app.use('/api', require('./routes/adverts.routes'));
 app.use('/auth', require('./routes/auth.routes'));
 
-/*app.get('*', (req, res) => {
+// serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});*/
+});
 
 app.use((req, res) => {
     res.status(404).send({ message: "Not found..." });
