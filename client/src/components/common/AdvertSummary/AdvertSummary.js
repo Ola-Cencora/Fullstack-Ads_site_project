65554b349fbf2a90e4d39f0dcfd09798c989bea2
Ola <cencora.aleksandra@gmail.com>
2024-03-08 11:52:57 +0100

    Make imgs on main page clickable

--- client/src/components/common/AdvertSummary/AdvertSummary.js
+++ client/src/components/common/AdvertSummary/AdvertSummary.js
@@ -2,14 +2,21 @@ import { Card, Row, Col } from "react-bootstrap";
 import styles from "./AdvertSummary.module.scss";
 import { IMGS_URL } from "../../../config";
 import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Button from "../Button/Button";
 
 const AdvertSummary = ({ id, title, location, price, img }) => {
   return (
     <Col sm={12} lg={6} xl={4}>
       <Card className={styles.summaryBox}>
-        <Card.Img variant="top" src={IMGS_URL + img} alt={title} className={styles.summaryBox__img} />
+        <Link to={`/adverts/${id}`}>
+          <Card.Img
+            variant="top"
+            src={IMGS_URL + img}
+            alt={title}
+            className={styles.summaryBox__img}
+          />
+        </Link>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Row className={styles.summaryBox__details}>
@@ -34,7 +41,7 @@ AdvertSummary.propTypes = {
   title: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  img: PropTypes.string.isRequired
-}
+  img: PropTypes.string.isRequired,
+};
 
 export default AdvertSummary;
