e87e71a31a1e5f6054f846e4ed52d3f1b636cc2a
Ola <cencora.aleksandra@gmail.com>
2024-03-08 17:02:19 +0100

    Connect to Atlas db

--- db.js
+++ db.js
@@ -1,18 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectToDB = () => {
+  const dbURI =
+    process.env.NODE_ENV === "production"
+      ? `mongodb+srv://olacencora:${process.env.DB_PASS}@clusteradverts.hukizlo.mongodb.net/MessageBoardDB?retryWrites=true&w=majority&appName=ClusterAdverts`
+      : "mongodb://0.0.0.0:27017/MessageBoardDB";
 
   // connect to DB
-  mongoose.connect('mongodb://0.0.0.0:27017/MessageBoardDB', {});
+  mongoose.connect(dbURI, {});
   const db = mongoose.connection;
 
   // on success
-  db.once('open', () => {
-    console.log('Connected to the database');
+  db.once("open", () => {
+    console.log("Connected to the database");
   });
 
   // on error
-  db.on('error', (err) => console.log('Error ' + err));
-}
+  db.on("error", (err) => console.log("Error " + err));
+};
 
 module.exports = connectToDB;
